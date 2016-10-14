b3ca9a614bba6890986770e1e7ffc34f2c8a97b7
KQ <eternal.september@gmail.com>
2016-10-13 21:49:39 -0400

    yay union all

--- 03_aggregation.sql
+++ 03_aggregation.sql
@@ -138,7 +138,20 @@ LIMIT 1;
 Produce a list of the total number of slots booked per facility per month in the year of 2012. In this version, include output rows containing totals for all months per facility, and a total for all months for all facilities. The output table should consist of facility id, month and slots, sorted by the id and month. When calculating the aggregated values for all months and all facids, return null values in the month and facid columns.
 */
 
-
+SELECT facid, EXTRACT(month FROM starttime) AS month, SUM(slots) AS sum
+	FROM cd.bookings
+	WHERE EXTRACT(year FROM starttime) = 2012
+	GROUP BY facid, month
+UNION ALL
+SELECT facid, NULL, SUM(slots) AS sum
+	FROM cd.bookings
+	WHERE EXTRACT(year FROM starttime) = 2012
+	GROUP BY facid
+UNION ALL
+SELECT NULL, NULL, SUM(slots) AS sum
+	FROM cd.bookings
+	WHERE EXTRACT(year FROM starttime) = 2012
+ORDER BY facid, month
 
 
 
