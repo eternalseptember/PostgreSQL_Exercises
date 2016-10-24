e9096501a921f735a43f8b563bd1e0045cdbeffb
KQ <eternal.september@gmail.com>
2016-10-24 12:46:16 -0400

    quick recursive question

--- 06_recursive.sql
+++ 06_recursive.sql
@@ -21,3 +21,34 @@ ORDER BY recommender DESC
 
 
 
+/*
+Find the downward recommendation chain for member ID 1: that is, the members they recommended, the members those members recommended, and so on. Return member ID and name, and order by ascending member id.
+*/
+
+WITH RECURSIVE rec_chain AS (
+	SELECT memid
+	FROM cd.members
+	WHERE recommendedby = 1
+	UNION
+	SELECT mem.memid
+	FROM cd.members AS mem
+	JOIN rec_chain AS r
+		ON r.memid = mem.recommendedby
+)
+
+SELECT rec.memid, mem.firstname, mem.surname
+FROM rec_chain AS rec
+JOIN cd.members AS mem
+	ON rec.memid = mem.memid
+ORDER BY memid ASC
+
+
+
+/*
+Produce a CTE that can return the upward recommendation chain for any member. You should be able to select recommender from recommenders where member=x. Demonstrate it by getting the chains for members 12 and 22. Results table should have member and recommender, ordered by member ascending, recommender descending.
+*/
+
+
+
+
+
